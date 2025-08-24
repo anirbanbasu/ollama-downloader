019a4bc87efad1e4f8e06e0acf5b4c51a5eba8a5
Jairo Lopez <nacarino@gmail.com>
2025-08-25 03:45:56 +0900

    chore: test cases for the OllamaServer BaseModel

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 from ollama_downloader import data_models
 
-def test_default_ollama_server_valid_url():    
+def test_ollama_server_default():
     server_model = data_models.OllamaServer()
 
 def test_ollama_server_valid_url():
@@ -10,3 +10,15 @@ def test_ollama_server_valid_url():
 def test_ollama_server_invalid_url():
     with pytest.raises(ValueError):
         server_model = data_models.OllamaServer(url="http://:11434")
+
+def test_ollama_server_assign_valid_url():
+    server_model = data_models.OllamaServer()
+
+    server_model.url = "http://192.168.1.1:11434"
+    server_model.url = "https://mypage.com:11434"
+
+def test_ollama_server_assign_invalid_url():
+    server_model = data_models.OllamaServer()
+
+    with pytest.raises(ValueError):
+        server_model.url = "http://:11434"
