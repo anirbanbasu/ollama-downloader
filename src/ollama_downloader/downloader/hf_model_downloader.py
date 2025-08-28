d8552ef601592af136a780bbc41e4fc6e0e7908d
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-08-29 07:19:35 +0900

    chore: More refactoring to move common functionality to the downloader abstract base class.

--- src/ollama_downloader/downloader/hf_model_downloader.py
+++ src/ollama_downloader/downloader/hf_model_downloader.py
@@ -1,27 +1,16 @@
 import datetime
-import hashlib
 import logging
-import os
-import shutil
-import tempfile
 from typing import List, Tuple
 
 from environs import env
 
 from ollama_downloader.common import EnvVar
-from ollama_downloader.data.data_models import AppSettings, ImageManifest
-from ollama_downloader.downloader.downloader import Downloader
+from ollama_downloader.data.data_models import ImageManifest
+from ollama_downloader.downloader.downloader import Downloader, ModelSource
 
 # import lxml.html
 from ollama import Client as OllamaClient
 
-from rich.progress import (
-    Progress,
-    BarColumn,
-    TextColumn,
-    DownloadColumn,
-    TransferSpeedColumn,
-)
 
 # Initialize the logger
 logger = logging.getLogger(__name__)
@@ -29,224 +18,15 @@ logger.setLevel(env.str(EnvVar.LOG_LEVEL, default=EnvVar.DEFAULT__LOG_LEVEL).upp
 
 
 class HuggingFaceModelDownloader(Downloader):
-    HF_BASE_HOST = "hf.co"
-    HF_BASE_URL = f"https://{HF_BASE_HOST}/v2/"
+    def __init__(self):
+        super().__init__()
 
-    def __init__(self, settings: AppSettings | None = None):
-        """
-        Initialize the model downloader with application settings.
-
-        Args:
-            settings (AppSettings | None): The application settings. If None, defaults will be used.
-        """
-        self.settings = settings or AppSettings.load_or_create_default()
-        if not self.settings:
-            raise RuntimeError("Failed to load or create and save default settings.")
-
-    def _get_manifest_url(self, user_repo_quant: str) -> str:
-        """
-        Construct the URL for a model manifest based on its name and tag.
-
-        Args:
-            user_repo_quant (str): The model identifier in the format "username/repository:quantisation".
-
-        Returns:
-            str: The URL for the model manifest.
-        """
-        logger.debug(f"Constructing manifest URL for {user_repo_quant}")
-        return f"{HuggingFaceModelDownloader.HF_BASE_URL}{user_repo_quant.replace(':', '/manifests/')}"
-
-    def _get_blob_url(self, user_repo_quant: str, digest: str) -> str:
-        """
-        Construct the URL for a BLOB based on its digest.
-
-        Args:
-            user_repo_quant (str): The model identifier in the format "username/repository:quantisation".
-            digest (str): The digest of the BLOB prefixed with the digest algorithm followed by a colon character.
-
-        Returns:
-            str: The URL for the BLOB.
-        """
-        logger.debug(
-            f"Constructing BLOB URL for {user_repo_quant} with digest {digest}"
-        )
-        return f"{HuggingFaceModelDownloader.HF_BASE_URL}{user_repo_quant.split(':')[0]}/blobs/{digest}"
-
-    def _fetch_manifest(self, user_repo_quant: str) -> str:
-        """
-        Fetch the manifest for a model from the Ollama registry.
-
-        Args:
-            user_repo_quant (str): The model identifier in the format "username/repository:quantisation".
-
-        Returns:
-            str: The JSON string of the model manifest.
-        """
-        url = self._get_manifest_url(user_repo_quant=user_repo_quant)
-        logger.info(
-            f"Downloading manifest for [bold cyan]{user_repo_quant}[/bold cyan]"
-        )
-        with self.get_httpx_client(
-            self.settings.ollama_library.verify_ssl,
-            self.settings.ollama_library.timeout,
-        ) as http_client:
-            response = http_client.get(url)
-            response.raise_for_status()
-            return response.text
-
-    def _download_model_blob(self, user_repo_quant: str, named_digest: str) -> tuple:
-        """
-        Download a file given the digest and save it to the specified destination.
-
-        Args:
-            user_repo_quant (str): The model identifier in the format "username/repository:quantisation".
-            named_digest (str): The digest of the BLOB prefixed with the digest algorithm followed by a colon character.
-
-        Returns:
-            tuple: A tuple containing the path to the downloaded file and its computed SHA256 digest.
-        """
-        url = self._get_blob_url(user_repo_quant=user_repo_quant, digest=named_digest)
-        # try:
-        sha256_hash = hashlib.new("sha256")
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            self._unnecessary_files.add(temp_file.name)
-            with self.get_httpx_client(
-                self.settings.ollama_library.verify_ssl,
-                self.settings.ollama_library.timeout,
-            ).stream("GET", url) as response:
-                response.raise_for_status()
-                total = int(response.headers["Content-Length"])
-
-                with Progress(
-                    TextColumn(text_format="[bold blue]{task.description}[/bold blue]"),
-                    "[progress.percentage]{task.percentage:>3.0f}%",
-                    BarColumn(bar_width=None),
-                    DownloadColumn(),
-                    TransferSpeedColumn(),
-                ) as progress:
-                    download_task = progress.add_task(
-                        f"Downloading BLOB {named_digest[:11]}...{named_digest[-4:]}",
-                        total=total,
-                    )
-                    for chunk in response.iter_bytes():
-                        sha256_hash.update(chunk)
-                        temp_file.write(chunk)
-                        progress.update(
-                            download_task, completed=response.num_bytes_downloaded
-                        )
-        logger.debug(f"Downloaded {url} to {temp_file.name}")
-        content_digest = sha256_hash.hexdigest()
-        logger.debug(f"Computed SHA256 digest of {temp_file.name}: {content_digest}")
-        return temp_file.name, content_digest
-
-    def _save_manifest_to_destination(
-        self,
-        data: str,
-        user_repo_quant: str,
-    ) -> str:
-        """
-        Copy a manifest data to destination.
-
-        Args:
-            data (str): The JSON string of the manifest.
-            user_repo_quant (str): The model identifier in the format "username/repository:quantisation".
-
-        Returns:
-            str: The path to the saved manifest file.
-        """
-        manifests_toplevel_dir = os.path.join(
-            (
-                os.path.expanduser(self.settings.ollama_library.models_path)
-                if self.settings.ollama_library.models_path.startswith("~")
-                else self.settings.ollama_library.models_path
-            ),
-            "manifests",
-        )
-        org_repo_model_splits = user_repo_quant.split(":")
-        manifests_dir = os.path.join(
-            manifests_toplevel_dir,
-            HuggingFaceModelDownloader.HF_BASE_HOST,
-            org_repo_model_splits[0],
-        )
-        if not os.path.exists(manifests_dir):
-            logger.warning(
-                f"Manifests path {manifests_dir} does not exist. Will attempt to create it."
-            )
-            os.makedirs(manifests_dir)
-            self._unnecessary_files.add(manifests_dir)
-        target_file = os.path.join(manifests_dir, org_repo_model_splits[1])
-        with open(target_file, "w") as f:
-            f.write(data)
-            logger.info(f"Saved manifest to {target_file}")
-        if self.settings.ollama_library.user_group:
-            user, group = self.settings.ollama_library.user_group
-            shutil.chown(target_file, user, group)
-            # The directory ownership must also be changed because it may have been created by a different user, most likely a sudoer
-            # TODO: Is this necessary or can the ownership change to the top-level directory cascade down?
-            shutil.chown(manifests_dir, user, group)
-            shutil.chown(manifests_toplevel_dir, user, group)
-            logger.info(
-                f"Changed ownership of {target_file} to user: {user}, group: {group}"
-            )
-        self._unnecessary_files.add(target_file)
-        return target_file
-
-    def _copy_blob_to_destination(
-        self,
-        source: str,
-        named_digest: str,
-        computed_digest: str,
-    ) -> tuple:
-        """
-        Copy a downloaded BLOB to the destination and verify its digest.
-
-        Args:
-            source (str): The path to the downloaded BLOB.
-            named_digest (str): The expected digest of the BLOB prefixed with the digest algorithm followed by the colon character.
-            computed_digest (str): The computed digest of the BLOB.
-
-        Returns:
-            tuple: A tuple containing a boolean indicating success and the path to the copied file.
-        """
-        if computed_digest != named_digest[7:]:
-            logger.error(
-                f"Digest mismatch: expected {named_digest[7:]}, got {computed_digest}"
-            )
-            return False, None
-        blobs_dir = os.path.join(
-            (
-                os.path.expanduser(self.settings.ollama_library.models_path)
-                if self.settings.ollama_library.models_path.startswith("~")
-                else self.settings.ollama_library.models_path
-            ),
-            "blobs",
-        )
-        logger.info(f"BLOB {named_digest} digest verified successfully.")
-        if not os.path.isdir(blobs_dir):
-            logger.error(f"BLOBS path {blobs_dir} must be a directory.")
-            return False, None
-        if not os.path.exists(blobs_dir):
-            logger.error(f"BLOBS path {blobs_dir} must exist.")
-            return False, None
-        target_file = os.path.join(blobs_dir, named_digest.replace(":", "-"))
-        shutil.move(source, target_file)
-        logger.info(f"Moved {source} to {target_file}")
-        if self.settings.ollama_library.user_group:
-            user, group = self.settings.ollama_library.user_group
-            shutil.chown(target_file, user, group)
-            shutil.chown(blobs_dir, user, group)
-            # Set permissions to rw-r-----
-            os.chmod(target_file, 0o640)
-            logger.info(
-                f"Changed ownership of {target_file} to user: {user}, group: {group}"
-            )
-        self._unnecessary_files.remove(source)
-        self._unnecessary_files.add(target_file)
-        return True, target_file
-
-    def download_model(self, model_identifier: str) -> None:
+    def download_model(self, model_identifier: str) -> bool:
         # Validate the response as an ImageManifest but don't enforce strict validation
-        manifest_json = self._fetch_manifest(user_repo_quant=model_identifier)
+        print(f"Downloading Hugging Face model {model_identifier}")
+        manifest_json = self._fetch_manifest(
+            model_identifier=model_identifier, model_source=ModelSource.HUGGINGFACE
+        )
         logger.debug(f"Validating manifest for {model_identifier}")
         manifest = ImageManifest.model_validate_json(manifest_json, strict=False)
         logger.info(
@@ -258,8 +38,9 @@ class HuggingFaceModelDownloader(Downloader):
         files_to_be_copied: List[Tuple[str, str, str]] = []
         # Download the model configuration BLOB
         file_model_config, digest_model_config = self._download_model_blob(
-            user_repo_quant=model_identifier,
+            model_identifier=model_identifier,
             named_digest=manifest.config.digest,
+            model_source=ModelSource.HUGGINGFACE,
         )
         files_to_be_copied.append(
             (file_model_config, manifest.config.digest, digest_model_config)
@@ -272,13 +53,14 @@ class HuggingFaceModelDownloader(Downloader):
                 f"Downloading [bold cyan]{layer.mediaType}[/bold cyan] layer [bold cyan]{layer.digest}[/bold cyan]"
             )
             file_layer, digest_layer = self._download_model_blob(
-                user_repo_quant=model_identifier,
+                model_identifier=model_identifier,
                 named_digest=layer.digest,
+                model_source=ModelSource.HUGGINGFACE,
             )
             files_to_be_copied.append((file_layer, layer.digest, digest_layer))
         # All BLOBs have been downloaded, now copy them to their appropriate destinations.
         for source, named_digest, computed_digest in files_to_be_copied:
-            copy_status, copy_destination = self._copy_blob_to_destination(
+            copy_status, copy_destination = self._save_blob(
                 source=source,
                 named_digest=named_digest,
                 computed_digest=computed_digest,
@@ -288,9 +70,10 @@ class HuggingFaceModelDownloader(Downloader):
                     f"Failed to copy {named_digest} to {copy_destination}."
                 )
         # Finally, save the manifest to its appropriate destination
-        self._save_manifest_to_destination(
+        self._save_manifest(
             data=manifest_json,
-            user_repo_quant=model_identifier,
+            model_identifier=model_identifier,
+            model_source=ModelSource.HUGGINGFACE,
         )
         ts_approximate_manifest_save = datetime.datetime.now()
         # Finally check if it exists in the Ollama
@@ -327,3 +110,4 @@ class HuggingFaceModelDownloader(Downloader):
             )
         # If we reached here cleanly, remove all unnecessary file names but don't remove actual files.
         self._unnecessary_files.clear()
+        return found_model
--- src/ollama_downloader/downloader/model_downloader.py
+++ src/ollama_downloader/downloader/model_downloader.py
@@ -1,20 +1,16 @@
 import datetime
-import hashlib
 import json
 import logging
 import os
-import shutil
-import tempfile
 from typing import Dict, List, Set, Tuple
-from urllib.parse import urlparse
 
 from httpx import URL
 
 from environs import env
 
 from ollama_downloader.common import EnvVar
-from ollama_downloader.data.data_models import AppSettings, ImageManifest
-from ollama_downloader.downloader.downloader import Downloader
+from ollama_downloader.data.data_models import ImageManifest
+from ollama_downloader.downloader.downloader import Downloader, ModelSource
 import lxml.html
 from ollama import Client as OllamaClient
 
@@ -24,8 +20,6 @@ from rich.progress import (
     Progress,
     BarColumn,
     TextColumn,
-    DownloadColumn,
-    TransferSpeedColumn,
     MofNCompleteColumn,
 )
 
@@ -35,173 +29,10 @@ logger.setLevel(env.str(EnvVar.LOG_LEVEL, default=EnvVar.DEFAULT__LOG_LEVEL).upp
 
 
 class OllamaModelDownloader(Downloader):
-    def __init__(self, settings: AppSettings | None = None):
-        """
-        Initialize the model downloader with application settings.
-
-        Args:
-            settings (AppSettings | None): The application settings. If None, defaults will be used.
-        """
-        self.settings = settings or AppSettings.load_or_create_default()
-        if not self.settings:
-            raise RuntimeError("Failed to load or create and save default settings.")
+    def __init__(self):
+        super().__init__()
         self.models_tags: Dict[str, list] = {}
         self.library_models: List[str] = []
-
-    def _get_manifest_url(self, model: str, tag: str) -> URL:
-        """
-        Construct the URL for a model manifest based on its name and tag.
-
-        Args:
-            model (str): The name of the model, e.g., llama3.1.
-            tag (str): The tag of the model, e.g., latest.
-
-        Returns:
-            URL: The URL for the model manifest.
-        """
-        logger.debug(f"Constructing manifest URL for {model}:{tag}")
-        return URL(self.settings.ollama_library.registry_base_url).join(
-            f"{model}/manifests/{tag}"
-        )
-
-    def _get_blob_url(self, model: str, digest: str) -> URL:
-        """
-        Construct the URL for a BLOB based on its digest.
-
-        Args:
-            model (str): The name of the model, e.g., llama3.1.
-            digest (str): The digest of the BLOB prefixed with the digest algorithm followed by a colon character.
-
-        Returns:
-            URL: The URL for the BLOB.
-        """
-        logger.debug(f"Constructing BLOB URL for {model} with digest {digest}")
-        return URL(self.settings.ollama_library.registry_base_url).join(
-            f"{model}/blobs/{digest.replace(':', '-')}"
-        )
-
-    def _fetch_manifest(self, model: str, tag: str) -> str:
-        """
-        Fetch the manifest for a model from the Ollama registry.
-
-        Args:
-            model (str): The name of the model, e.g., llama3.1.
-            tag (str): The tag of the model, e.g., latest.
-
-        Returns:
-            str: The JSON string of the model manifest.
-        """
-        url = self._get_manifest_url(model=model, tag=tag)
-        logger.info(f"Downloading manifest for [bold cyan]{model}:{tag}[/bold cyan]")
-        with self.get_httpx_client(
-            self.settings.ollama_library.verify_ssl,
-            self.settings.ollama_library.timeout,
-        ) as http_client:
-            response = http_client.get(url)
-            response.raise_for_status()
-            return response.text
-
-    def _download_model_blob(self, model: str, named_digest: str) -> tuple:
-        """
-        Download a file given the digest and save it to the specified destination.
-
-        Args:
-            model (str): The name of the model, e.g., llama3.1.
-            named_digest (str): The digest of the BLOB prefixed with the digest algorithm followed by a colon character.
-
-        Returns:
-            tuple: A tuple containing the path to the downloaded file and its computed SHA256 digest.
-        """
-        url = self._get_blob_url(model=model, digest=named_digest)
-        # try:
-        sha256_hash = hashlib.new("sha256")
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            self._unnecessary_files.add(temp_file.name)
-            with self.get_httpx_client(
-                self.settings.ollama_library.verify_ssl,
-                self.settings.ollama_library.timeout,
-            ).stream("GET", url) as response:
-                response.raise_for_status()
-                total = int(response.headers["Content-Length"])
-
-                with Progress(
-                    TextColumn(text_format="[bold blue]{task.description}[/bold blue]"),
-                    "[progress.percentage]{task.percentage:>3.0f}%",
-                    BarColumn(bar_width=None),
-                    DownloadColumn(),
-                    TransferSpeedColumn(),
-                ) as progress:
-                    download_task = progress.add_task(
-                        f"Downloading BLOB {named_digest[:11]}...{named_digest[-4:]}",
-                        total=total,
-                    )
-                    for chunk in response.iter_bytes():
-                        sha256_hash.update(chunk)
-                        temp_file.write(chunk)
-                        progress.update(
-                            download_task, completed=response.num_bytes_downloaded
-                        )
-        logger.debug(f"Downloaded {url} to {temp_file.name}")
-        content_digest = sha256_hash.hexdigest()
-        logger.debug(f"Computed SHA256 digest of {temp_file.name}: {content_digest}")
-        return temp_file.name, content_digest
-
-    def _save_manifest_to_destination(
-        self,
-        data: str,
-        model: str,
-        tag: str,
-    ) -> str:
-        """
-        Copy a manifest data to destination.
-
-        Args:
-            data (str): The JSON string of the manifest.
-            model (str): The name of the model, e.g., llama3.1.
-            tag (str): The tag of the model, e.g., latest.
-
-        Returns:
-            str: The path to the saved manifest file.
-        """
-        ollama_registry_host = urlparse(
-            self.settings.ollama_library.registry_base_url
-        ).hostname
-        manifests_toplevel_dir = os.path.join(
-            (
-                os.path.expanduser(self.settings.ollama_library.models_path)
-                if self.settings.ollama_library.models_path.startswith("~")
-                else self.settings.ollama_library.models_path
-            ),
-            "manifests",
-        )
-        manifests_dir = os.path.join(
-            manifests_toplevel_dir,
-            ollama_registry_host,
-            "library",
-            model,
-        )
-        if not os.path.exists(manifests_dir):
-            logger.warning(
-                f"Manifests path {manifests_dir} does not exist. Will attempt to create it."
-            )
-            os.makedirs(manifests_dir)
-            self._unnecessary_files.add(manifests_dir)
-        target_file = os.path.join(manifests_dir, tag)
-        with open(target_file, "w") as f:
-            f.write(data)
-            logger.info(f"Saved manifest to {target_file}")
-        if self.settings.ollama_library.user_group:
-            user, group = self.settings.ollama_library.user_group
-            shutil.chown(target_file, user, group)
-            # The directory ownership must also be changed because it may have been created by a different user, most likely a sudoer
-            # TODO: Is this necessary or can the ownership change to the top-level directory cascade down?
-            shutil.chown(manifests_dir, user, group)
-            shutil.chown(manifests_toplevel_dir, user, group)
-            logger.info(
-                f"Changed ownership of {target_file} to user: {user}, group: {group}"
-            )
-        self._unnecessary_files.add(target_file)
-        return target_file
 
     def _save_models_tags_cache(
         self,
@@ -231,67 +62,17 @@ class OllamaModelDownloader(Downloader):
                 f"No models tags cache found at {self.settings.ollama_library.models_tags_cache}"
             )
 
-    def _copy_blob_to_destination(
-        self,
-        source: str,
-        named_digest: str,
-        computed_digest: str,
-    ) -> tuple:
-        """
-        Copy a downloaded BLOB to the destination and verify its digest.
-
-        Args:
-            source (str): The path to the downloaded BLOB.
-            named_digest (str): The expected digest of the BLOB prefixed with the digest algorithm followed by the colon character.
-            computed_digest (str): The computed digest of the BLOB.
-
-        Returns:
-            tuple: A tuple containing a boolean indicating success and the path to the copied file.
-        """
-        if computed_digest != named_digest[7:]:
-            logger.error(
-                f"Digest mismatch: expected {named_digest[7:]}, got {computed_digest}"
-            )
-            return False, None
-        blobs_dir = os.path.join(
-            (
-                os.path.expanduser(self.settings.ollama_library.models_path)
-                if self.settings.ollama_library.models_path.startswith("~")
-                else self.settings.ollama_library.models_path
-            ),
-            "blobs",
-        )
-        logger.info(f"BLOB {named_digest} digest verified successfully.")
-        if not os.path.isdir(blobs_dir):
-            logger.error(f"BLOBS path {blobs_dir} must be a directory.")
-            return False, None
-        if not os.path.exists(blobs_dir):
-            logger.error(f"BLOBS path {blobs_dir} must exist.")
-            return False, None
-        target_file = os.path.join(blobs_dir, named_digest.replace(":", "-"))
-        shutil.move(source, target_file)
-        logger.info(f"Moved {source} to {target_file}")
-        if self.settings.ollama_library.user_group:
-            user, group = self.settings.ollama_library.user_group
-            shutil.chown(target_file, user, group)
-            shutil.chown(blobs_dir, user, group)
-            # Set permissions to rw-r-----
-            os.chmod(target_file, 0o640)
-            logger.info(
-                f"Changed ownership of {target_file} to user: {user}, group: {group}"
-            )
-        self._unnecessary_files.remove(source)
-        self._unnecessary_files.add(target_file)
-        return True, target_file
-
-    def download_model(self, model_identifier: str) -> None:
+    def download_model(self, model_identifier: str) -> bool:
         model, tag = (
             model_identifier.split(":")
             if ":" in model_identifier
             else (model_identifier, "latest")
         )
+        print(f"Downloading Ollama library model {model_identifier}")
         # Validate the response as an ImageManifest but don't enforce strict validation
-        manifest_json = self._fetch_manifest(model=model, tag=tag)
+        manifest_json = self._fetch_manifest(
+            model_identifier=model_identifier, model_source=ModelSource.OLLAMA
+        )
         logger.debug(f"Validating manifest for {model}:{tag}")
         manifest = ImageManifest.model_validate_json(manifest_json, strict=False)
         logger.info(
@@ -303,8 +84,9 @@ class OllamaModelDownloader(Downloader):
         files_to_be_copied: List[Tuple[str, str, str]] = []
         # Download the model configuration BLOB
         file_model_config, digest_model_config = self._download_model_blob(
-            model=model,
+            model_identifier=model_identifier,
             named_digest=manifest.config.digest,
+            model_source=ModelSource.OLLAMA,
         )
         files_to_be_copied.append(
             (file_model_config, manifest.config.digest, digest_model_config)
@@ -317,13 +99,14 @@ class OllamaModelDownloader(Downloader):
                 f"Downloading [bold cyan]{layer.mediaType}[/bold cyan] layer [bold cyan]{layer.digest}[/bold cyan]"
             )
             file_layer, digest_layer = self._download_model_blob(
-                model=model,
+                model_identifier=model_identifier,
                 named_digest=layer.digest,
+                model_source=ModelSource.OLLAMA,
             )
             files_to_be_copied.append((file_layer, layer.digest, digest_layer))
         # All BLOBs have been downloaded, now copy them to their appropriate destinations.
         for source, named_digest, computed_digest in files_to_be_copied:
-            copy_status, copy_destination = self._copy_blob_to_destination(
+            copy_status, copy_destination = self._save_blob(
                 source=source,
                 named_digest=named_digest,
                 computed_digest=computed_digest,
@@ -333,10 +116,10 @@ class OllamaModelDownloader(Downloader):
                     f"Failed to copy {named_digest} to {copy_destination}."
                 )
         # Finally, save the manifest to its appropriate destination
-        self._save_manifest_to_destination(
+        self._save_manifest(
             data=manifest_json,
-            model=model,
-            tag=tag,
+            model_identifier=model_identifier,
+            model_source=ModelSource.OLLAMA,
         )
         ts_approximate_manifest_save = datetime.datetime.now()
         # Finally check if it exists in the Ollama
@@ -372,6 +155,8 @@ class OllamaModelDownloader(Downloader):
             )
         # If we reached here cleanly, remove all unnecessary file names but don't remove actual files.
         self._unnecessary_files.clear()
+        # Finally return success
+        return True
 
     def update_models_list(self) -> list:
         """
