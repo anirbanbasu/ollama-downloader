1be11fea06bc997869bec025b5a5b402598a4b83
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-09-06 10:50:17 +0900

    fix: Using enum for environment variables is not a good idea to parse the default values.

--- src/ollama_downloader/common.py
+++ src/ollama_downloader/common.py
@@ -1,19 +1,9 @@
-from enum import Enum, auto
-
-
-class AllCapsStrEnum(str, Enum):
-    # See https://github.com/python/cpython/issues/115509#issuecomment-1946971056
-    @staticmethod
-    def _generate_next_value_(name, *args):
-        return name.upper()
-
-
-class EnvVar(AllCapsStrEnum):
-    LOG_LEVEL = auto()
+class EnvVar:
+    LOG_LEVEL = "LOG_LEVEL"
     DEFAULT__LOG_LEVEL = "INFO"
 
-    OD_UA_NAME_VER = auto()
+    OD_UA_NAME_VER = "OD_UA_NAME_VER"
     DEFAULT__OD_UA_NAME_VER = "ollama-downloader/0.1.0"
 
-    OD_SETTINGS_FILE = auto()
+    OD_SETTINGS_FILE = "OD_SETTINGS_FILE"
     DEFAULT__OD_SETTINGS_FILE = "conf/settings.json"
